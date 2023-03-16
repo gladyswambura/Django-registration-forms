e6b3ce8f228e2c0c69650a322380d4ea353e486b
gladys <gladyswahito7@gmail.com>
2023-03-16 08:36:40 +0300

    creates a profile and edit profile routes

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,8 @@ from .views import *
 
 urlpatterns = [
 path('', views.homepage, name='homepage'),
-path("signup/", SignUpView.as_view(), name="signup"),
+path("signup/", views.SignUpView, name="signup"),
 path('login/', LoginView.as_view(), name='login'),
+path('accounts/profile/', views.profile, name='profile'),
+path('accounts/profile/', views.profile_edit, name='profile_edit'),
 ]
