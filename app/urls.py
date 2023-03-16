eda2f8272c9d05eef9425a2b63052734235a20ac
gladys <gladyswahito7@gmail.com>
2023-03-16 09:00:51 +0300

    creates the form's route

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,15 @@
 from  django.urls  import  path
 from . import views 
 from .views import *
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
 path('', views.homepage, name='homepage'),
 path("signup/", views.SignUpView, name="signup"),
 path('login/', LoginView.as_view(), name='login'),
+path('form/', views.form, name='form'),
 path('accounts/profile/', views.profile, name='profile'),
-path('accounts/profile/', views.profile_edit, name='profile_edit'),
+path('accounts/edit_profile/', views.profile_edit, name='profile_edit'),
+  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
